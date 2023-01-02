5a0a4aa5a48dda49ee7c454be3b197e75ec3fa64
carlynorama <carlynorama@gmail.com>
2023-01-01 16:02:55 -0800

    faster

--- converters/markdown2html.py
+++ converters/markdown2html.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-#https://www.digitalocean.com/community/tutorials/how-to-use-python-markdown-to-convert-markdown-text-to-html
-# python --version     <-- if not 3 add the three
-# python3 --version
-# python3 -m pip --version
-# python3 -m pip install markdown
-
 import markdown
 import sys
 
@@ -16,6 +10,10 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         mdFileInput = sys.argv[1]
         htmlFileOutput = sys.argv[2]
+    if len(sys.argv) == 2:
+        fileNameRoot = sys.argv[1]
+        mdFileInput = fileNameRoot + ".md"
+        htmlFileOutput = fileNameRoot + ".html"
     else:
         mdFileInput = input ("Source file: ")
         htmlFileOutput = input ("Destination file: ")
